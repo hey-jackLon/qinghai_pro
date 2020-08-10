c5cd0a58d8c65ef66dbf538f11b2868dbb3acf39
unknown <jacklon_66@163.com>
2020-08-10 10:24:59 +0800

    master

--- src/views/greateventsanalyze/table.vue
+++ src/views/greateventsanalyze/table.vue
@@ -4,7 +4,7 @@
       <el-table-column>
         <el-table-column prop="gcrq_str" label="时间" />
       </el-table-column>
-      <el-table-column label="机动车（辆/日）">
+      <el-table-column label="交通量（辆/日）">
         <el-table-column prop="jdc_dl" label="当量数" />
         <el-table-column label="同比">
           <template
@@ -17,7 +17,7 @@
           >{{ scope.row.jdc_dl_week_rate==9999?'暂无数据':scope.row.jdc_dl_week_rate }}</template>
         </el-table-column>
       </el-table-column>
-      <el-table-column label="货车（辆/日）">
+      <el-table-column label="汽车（辆/日）">
         <el-table-column prop="hc_dl" label="当量数" />
         <el-table-column label="同比">
           <template
@@ -43,6 +43,20 @@
           >{{ scope.row.kc_dl_week_rate==9999?'暂无数据':scope.row.kc_dl_week_rate }}</template>
         </el-table-column>
       </el-table-column>
+      <el-table-column label="货车（辆/日）">
+        <el-table-column prop="hc_dl" label="当量数" />
+        <el-table-column label="同比">
+          <template
+            slot-scope="scope"
+          >{{ scope.row.hc_dl_year_rate==9999?'暂无数据':scope.row.hc_dl_year_rate }}</template>
+        </el-table-column>
+        <el-table-column label="环比">
+          <template
+            slot-scope="scope"
+          >{{ scope.row.hc_dl_week_rate==9999?'暂无数据':scope.row.hc_dl_week_rate }}</template>
+        </el-table-column>
+      </el-table-column>
+
     </el-table>
   </div>
 </template>
